55fda40c97f26e09e38e0915ede37cd3f1a3f404
imgrizi <brunomiguel.lopessilva@gmail.com>
2023-03-27 22:27:16 +0100

    27/3

--- java exercicio 7/scripts2.js
+++ java exercicio 7/scripts2.js
@@ -1,12 +1,19 @@
-document.querySelector('input').addEventListener('focus',mostraConteudoInput)
-document.querySelector('input').addEventListener('blur',escondeConteudoInput)
+//document.querySelector('input').addEventListener('focus',mostraConteudoInput)
+//document.querySelector('input').addEventListener('blur',escondeConteudoInput)
+//usar # no lugar dos inputs
+document.querySelector('#nome').addEventListener('focus',mostraConteudoInput)
+document.querySelector('#nome').addEventListener('blur',escondeConteudoInput)
+
+document.querySelector('#email').addEventListener('focus',mostraConteudoInput)
+document.querySelector('#email').addEventListener('blur',escondeConteudoInput)
 
 function mostraConteudoInput(){
     //alert('entrei no add event listener');
     console.log('entrei no add listener');
-    
+
 }
 
 function escondeConteudoInput(){
 console.log('entrei no add listener PARA ESCONDER')
+console.log(this.value)
 }
