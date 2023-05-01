088d2a37771decc941eefaecec7272be4aa75fcc
imgrizi <117866162+imgrizi@users.noreply.github.com>
2023-05-01 16:11:06 +0100

    treino 1/5

---
+++ treino caixa de texto/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.caixa-texto').keyup(function(){
+        console.log('Hello there');
+        conteudo_textarea = $('.caixa-texto').val();
+        numero_de_caracteres = conteudo_textarea.length;
+        $('.contador').text(numero_de_caracteres);
+
+        if(numero_de_caracteres == 0 ){
+            $('.barra-progresso').removeClass('barra-verde');
+            
+
+        }
+        else if(numero_de_caracteres >= 1 && numero_de_caracteres <= 10)
+        {
+            
+            $('.barra-progresso').addClass('barra-verde');
+            $('.barra-progresso').removeClass('barra-azul');
+            
+        }
+        else if(numero_de_caracteres > 10 && numero_de_caracteres <= 20)
+        {
+            $('.barra-progresso').addClass('barra-azul');
+            $('.barra-progresso').removeClass('barra-vermelha');
+            
+        }
+       
+        else 
+        {
+            $('.barra-progresso').addClass('barra-vermelho');
+            
+            
+
+        }
+    });
+});
