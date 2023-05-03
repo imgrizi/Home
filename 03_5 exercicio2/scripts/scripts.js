b4feba4aae3b8157350562df3df1e917e1fc00e3
imgrizi <117866162+imgrizi@users.noreply.github.com>
2023-05-03 22:28:30 +0100

    03/05

---
+++ 03_5 exercicio2/scripts/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.onda').colorbox({
+    width:'100%',
+    rel:'onda',
+     });
+
+    $('.regista').colorbox({
+        width:400,
+        height:300,
+
+    })
+
+ 
+});
--- treino caixa de texto/js/scripts.js
+++ treino caixa de texto/js/scripts.js
@@ -4,10 +4,14 @@ $(document).ready(function(){
         conteudo_textarea = $('.caixa-texto').val();
         numero_de_caracteres = conteudo_textarea.length;
         $('.contador').text(numero_de_caracteres);
+        
 
         if(numero_de_caracteres == 0 ){
             $('.barra-progresso').removeClass('barra-verde');
-            
+           // $('.avalia').text('és um nabo');
+            mensagem = "es um nabo";
+            $('.contador').append(mensagem);
+
 
         }
         else if(numero_de_caracteres >= 1 && numero_de_caracteres <= 10)
@@ -15,12 +19,22 @@ $(document).ready(function(){
             
             $('.barra-progresso').addClass('barra-verde');
             $('.barra-progresso').removeClass('barra-azul');
+           // $('.avalia').text('és um bocado nabo');
+           mensagem = "es uma beca nabo";
+           //$('.contador').text(numero_de_caracteres+ mensagem);
+           $('.contador').append(mensagem);
+
             
         }
         else if(numero_de_caracteres > 10 && numero_de_caracteres <= 20)
         {
             $('.barra-progresso').addClass('barra-azul');
             $('.barra-progresso').removeClass('barra-vermelha');
+           // $('.avalia').text('és um nadinha nabo');
+           mensagem = "es uma beca pequena nabo";
+           //$('.contador').text(numero_de_caracteres+ mensagem);
+           $('.contador').append(mensagem);
+
             
         }
        
